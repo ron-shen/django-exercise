05714d5f7ae2bb9a03af11fe77548bad0faa8d23
ronshen0404 <ronshen0404@gmail.com>
2022-06-13 12:40:10 +0800

    db relationships

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Adress, Country
 
 # Register your models here.
 
@@ -9,6 +9,14 @@ class BookAdmin(admin.ModelAdmin):
   list_display = ("title", "author",)
   
   
+class AuthorAdmin(admin.ModelAdmin):
+  list_display = ("first_name", "last_name",)
   
+  
+  
+   
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Adress)
+admin.site.register(Country)
   
